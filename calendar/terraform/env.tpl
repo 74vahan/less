0a388e3c707b09eb2eea1a2c109f4a98bf4f0e01
74vahan <vsvan2004@gmail.com>
2026-06-26 21:59:06 +0400

    move calendar to its own repo (74vahan/calendar)

--- calendar/terraform/env.tpl
+++
@@ -1,5 +0,0 @@
-DB_USER=${db_user}
-DB_PASSWORD=${db_password}
-DB_NAME=${db_name}
-JWT_SECRET=${jwt_secret}
-HTTP_PORT=${http_port}
--- calendar/terraform/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "app_url" {
-  description = "URL приложения calendar"
-  value       = "http://${var.server_ip}:${var.http_port}"
-}
